52b1607b5804c352287211649eca65dfe63869a0
Dewald.Mouton <moutondewald@gmail.com>
2017-08-17 08:09:32 +0200

    navbar issues

--- main/js/younglings101Routing.js
+++ main/js/younglings101Routing.js
@@ -1,24 +1,24 @@
 var app = angular.module('main', ['ngRoute']);
-app.config(function ($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
-    .when('/twitter', {
-        templateUrl: "socialMedia/twitter.html"
-    })
-    .when('/facebook', {
-        templateUrl: "socialMedia/facebook.html"
-    })
-    .when('/instagram', {
-        templateUrl: "socialMedia/instagram.html"
-    })
-    .when('/youtube', {
-        templateUrl: "socialMedia/youtube.html"
-    })
-    .when('/linkedin', {
-        templateUrl: "socialMedia/linkedin.html"
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+        .when('/twitter', {
+            templateUrl: "socialMedia/twitter.html"
+        })
+        .when('/facebook', {
+            templateUrl: "socialMedia/facebook.html"
+        })
+        .when('/instagram', {
+            templateUrl: "socialMedia/instagram.html"
+        })
+        .when('/youtube', {
+            templateUrl: "socialMedia/youtube.html"
+        })
+        .when('/linkedin', {
+            templateUrl: "socialMedia/linkedin.html"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 })
 .controller('randomiser', ['$scope', '$sce', function ($scope, $sce) {
     $scope.links = [{ "name": "Ernst", "surname": "Kaese", "link": "./pages/younglingsProfilesSites/ernst/index.html", "src": "./pages/younglingsProfilesSites/ernst/img/faceshot.jpg", "display": "CYshow" }, { "name": "Malcolm", "surname": "Carelse", "link": "./pages/younglingsProfilesSites/malcolm/malcolmyoungling.html", "src": "./pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg", "display": "CYshow" }, { "name": "Athandile", "surname": "Tembile", "link": "./pages/younglingsProfilesSites/athandile/index.html", "src": "./pages/younglingsProfilesSites/athandile/Images/Head.jpg", "display": "CYshow" }, { "name": "Keenan", "surname": "Idas", "link": "./pages/younglingsProfilesSites/keenan/index.html", "src": "./pages/younglingsProfilesSites/keenan/images/Keenan_Idas.jpg", "display": "CYshow" }, { "name": "Zahirah", "surname": "Ismail", "link": "./pages/younglingsProfilesSites/zahirah/index.html", "src": "./pages/younglingsProfilesSites/zahirah/img/z3.jpg", "display": "CYshow" }, { "name": "Zafiera", "surname": "Davids", "link": "./pages/younglingsProfilesSites/zafiera/index.html", "src": "./pages/younglingsProfilesSites/zafiera/img/logo.png", "display": "CYshow" }, { "name": "Sixolile", "surname": "Mtengwana", "link": "./pages/younglingsProfilesSites/sixolile/index.html", "src": "./pages/younglingsProfilesSites/sixolile/img/sm.dp.jpg", "display": "CYshow" }, { "name": "Jean-Pierre", "surname": "Olwagen", "link": "./pages/younglingsProfilesSites/jean-pierre/index.html", "src": "./pages/younglingsProfilesSites/jean-pierre/profile picture.jpg", "display": "CYshow" }, { "name": "Dewald", "surname": "Mouton", "link": "./pages/younglingsProfilesSites/dewald/index.html", "src": "./pages/younglingsProfilesSites/dewald/images/profile.jpg", "display": "CYshow" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "./img/alienWorldp1.png", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "./img/alienWorldp2.png", "display": "CYhide" }, { "name": "", "surname": "", "link": "", "src": "./img/alienWorldp3.png", "display": "CYhide" }];
@@ -43,26 +43,26 @@ app.config(function ($routeProvider){
         }
     }
 
-    $scope.iframeMinimise = function(){
+    $scope.iframeMinimise = function () {
         $scope.setCurrentUrl("");
     }
 }]);
 
 function shuffle(array) {
-var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-// While there remain elements to shuffle...
-while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-}
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-return array;
+    return array;
 }
